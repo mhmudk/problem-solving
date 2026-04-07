e8685b58c8365ed1869d2196d285d51305111aaa
mahmoud <mahmoud.khattab@damen.com.eg>
2026-04-07 14:12:11 +0200

    Solve B.Basic Data Types problem

--- src/BasicDataTypes.java
+++ src/BasicDataTypes.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class BasicDataTypes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        long l = sc.nextLong();
+        char c = sc.next().charAt(0);
+        float f = sc.nextFloat();
+        double d = sc.nextDouble();
+        System.out.print(i + "\n" + l + "\n" + c + "\n" + f + "\n" + d);
+
+    }
 }
