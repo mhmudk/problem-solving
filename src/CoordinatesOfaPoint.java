5695cc39d5109bfcd308ca021dd3b4975f51a8fb
mhmudk <mahmoudatiakhattab123@gmail.com>
2026-04-11 23:04:38 +0200

    Not

---
+++ src/CoordinatesOfaPoint.java
@@ -0,0 +1,51 @@
+
+import java.util.Scanner;
+
+
+public class CoordinatesOfaPoint {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        long one = sc.nextLong();
+        long two = sc.nextLong();
+
+        // TODO (0,0 ---> origin )  ------  (number , 0  -----> Eixo X) ------ (0 ,number ----> Eixo Y )
+        // TODO (+,+     --->q1  ( +,-   ---> Q4 ) (   --->)
+
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+    double x, y;
+    cin >> x >> y;
+
+    if (x == 0 && y == 0) {
+        cout << "Origem";
+    }
+    else if (x == 0 && y != 0) {
+        cout << "Eixo Y";
+    }
+    else if (x != 0 && y == 0) {
+        cout << "Eixo X";
+    }
+    else if (x > 0 && y > 0) {
+        cout << "Q1";
+    }
+    else if (x < 0 && y > 0) {
+        cout << "Q2";
+    }
+    else if (x < 0 && y < 0) {
+        cout << "Q3";
+    }
+    else if (x > 0 && y < 0) {
+        cout << "Q4";
+    }
+
+    return 0;
+}
+ */
+    }
+}
+
+
