ff66ee302aa2ba727a5f4d53a98a98dfffd23bbb
mhmudk <mahmoudatiakhattab123@gmail.com>
2026-04-12 23:45:30 +0200

    Solve S - Interval  Problem

---
+++ src/Interval.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Interval {
+    public static void main(String args[]){
+        Scanner sc  = new Scanner(System.in);
+        double x = sc.nextDouble();
+
+        // TODO first interval [0,25]
+        if(x>=0 && x<=25){
+            System.out.println("Interval [0,25]");
+        }
+
+        // TODO second  interval (25,50]
+        else if(x>25 && x<=50){
+            System.out.println("Interval (25,50]");
+        }
+
+
+        // TODO third   interval (50,75]
+        else if(x>50 && x<=75){
+            System.out.println("Interval (50,75]");
+        }
+
+        // TODO third   interval (50,75]
+        else if(x>50 && x<=75){
+            System.out.println("Interval (50,75]");
+        }
+
+
+        // TODO fourth   interval (75,100]
+       else if(x>75 && x<=100){
+            System.out.println("Interval (75,100]");
+        } else {
+            System.out.println("Out of Intervals");
+        }
+
+
+
+
+
+
+    }
+
+}
